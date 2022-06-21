a2c52feeeb1210493cd6b618b0c3542626978408
Ingvar <imagineaclevernamehere@gmail.com>
2022-06-21 18:05:37 +0200

    Fix offset bug for "second byte".

--- pkg/display/ledmatrix.go
+++ pkg/display/ledmatrix.go
@@ -4,12 +4,12 @@ import (
 	"image/color"
 
 	"github.com/hajimehoshi/ebiten/v2"
-	// log "github.com/sirupsen/logrus"
+	log "github.com/sirupsen/logrus"
 
 	"github.com/vatine/cryptoclicker/pkg/mixer"
 )
 
-const ledSize = 10
+const ledSize = 8
 
 var led *ebiten.Image
 
@@ -19,11 +19,19 @@ func DrawMatrix(f *ButtonField, i *ebiten.Image) {
 
 	for ix, val := range bytes {
 		y := (ix / 2) * ledSize
-		xBase := (ix%2)*ledSize + 300
+		xBase := (ix%2)*8*ledSize + 290
 
 		for b := 0; b < 8; b++ {
 			mask := byte(1) << (7 - b)
-			x := xBase + (7-b)*ledSize
+			x := xBase + b*ledSize
+			log.WithFields(log.Fields{
+				"x":    x,
+				"y":    y,
+				"b":    b,
+				"mask": mask,
+				"val":  val,
+				"ix":   ix,
+			}).Debug("DrawMatrix")
 			if (val & mask) == mask {
 				var g ebiten.GeoM
 				g.Translate(float64(x), float64(y))
@@ -42,12 +50,13 @@ func init() {
 	ledCol := color.RGBA{R: 127, G: 255, B: 10, A: 255}
 	// blank := color.RGBA{R: 0, G: 0, B: 0, A: 0}
 
+	half := ledSize / 2
 	for x := 0; x < ledSize; x++ {
 		for y := 0; y < ledSize; y++ {
-			dx := x - 5
-			dy := y - 5
+			dx := x - half
+			dy := y - half
 
-			if (dx*dx)+(dy*dy) < 25 {
+			if (dx*dx)+(dy*dy) < (half * half) {
 				led.Set(x, y, ledCol)
 			}
 		}
